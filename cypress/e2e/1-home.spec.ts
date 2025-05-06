081089a3347a45f382fc0b09fa9a258caa15fae5
taczhed <patryklach99@gmail.com>
2025-05-06 10:59:59 +0200

    feat(home): home page visibility verification

--- cypress/e2e/1-home.spec.ts
+++ cypress/e2e/1-home.spec.ts
@@ -1,6 +1,27 @@
+import header from '../fixtures/header.json';
+
+const { navigation } = header;
+
 /*
     1. Home page visibility verification:
     1.1. Go to the home page
     1.2. Check if the logo is visible
     1.3. Check if the navigation buttons exists
 */
+
+describe('#1 | Home page visibility verification', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('should display the logo', () => {
+    cy.get('img[alt="Website for automation practice"]').should('be.visible');
+  });
+
+  it('should display the navigation buttons', () => {
+    cy.get('.navbar-nav')
+      .should('be.visible')
+      .find('a')
+      .each(($a, index) => cy.wrap($a).should('be.visible').and('contain.text', navigation[index].title));
+  });
+});
