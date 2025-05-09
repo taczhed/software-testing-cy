94e27ca797dd5a54971667f073845e887d1ee978
taczhed <patryklach99@gmail.com>
2025-05-09 12:52:29 +0200

    feat(contact): contact via form

--- cypress/e2e/4-contact.spec.ts
+++ cypress/e2e/4-contact.spec.ts
@@ -5,3 +5,32 @@
     8.3. Click send
     8.4. Check the success message
 */
+
+import user from '../fixtures/user.json';
+
+const { name, email } = user;
+
+describe('#8 | Contact via form', () => {
+  before(() => {
+    cy.visit('/');
+  });
+
+  it('should send a message via contact form and verify success message', () => {
+    // Home page
+    cy.get('a[href="/contact_us"]').should('be.visible').click();
+
+    // Contact page
+    cy.get('form#contact-us-form')
+      .should('be.visible')
+      .within(() => {
+        cy.get('input[data-qa="name"]').type(name);
+        cy.get('input[data-qa="email"]').type(email);
+        cy.get('input[data-qa="subject"]').type('Test subject');
+        cy.get('textarea[data-qa="message"]').type('Test message');
+
+        cy.get('input[data-qa="submit-button"]').click();
+      });
+
+    cy.get('div.status.alert-success').should('be.visible').and('contain.text', 'Success!');
+  });
+});
